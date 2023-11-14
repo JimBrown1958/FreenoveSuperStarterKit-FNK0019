790f76ab1dbdccda1243bb54d68c53f9e51c9b1d
JimBrown1958 <78706258+JimBrown1958@users.noreply.github.com>
2023-11-14 21:22:58 +0000

    Create 02.2.1_Tablelamp_gpois.fs
    gpios update

---
+++ 02.2.1_Tablelamp_gpois.fs
@@ -0,0 +1,63 @@
+#! /usr/bin/env gforth
+
+\ Forget and reload definitions if this file is re-included.
+[ifdef] -Tablelamp
+    -Tablelamp
+[endif]
+marker -Tablelamp
+
+include ./gpios.fs
+
+11 constant ledPin
+12 constant buttonPin
+50 constant captureTime 
+0 constant LOW
+1 constant HIGH
+
+
+variable ledState 
+variable buttonState 
+variable lastbuttonState 
+variable lastChangeTime 
+variable reading
+
+LOW ledState !  
+HIGH buttonState ! 
+HIGH lastbuttonState ! 
+
+ledPin output-pin
+buttonPin input-pin
+HIGH buttonPin pin-resmode
+
+: ledOn ( -- )      ledPin pinset ;
+: ledOff ( -- )     ledPin pinclr ;
+: buttonRead ( -- n ) buttonPin pin@ ;
+: ledSwitch  ledState @ 0= IF 1 ledState ! ELSE 0 ledState ! THEN ;
+
+: Tablelamp     ." Program is starting... " CR 
+		begin
+			buttonRead reading ! 
+			reading @ lastbuttonState @ <>  IF
+				millis lastChangeTime !
+			THEN
+
+			millis lastChangeTime @ - captureTime > IF
+				reading @ buttonState @ <> IF
+					reading @  buttonState !
+					buttonState @ LOW = IF
+						." Button is pressed!" CR
+						ledSwitch
+						ledState @  if
+							." turn on LED ... " CR
+						ELSE
+							." turn off LED ... " CR
+						THEN
+					THEN
+				ELSE
+					." Button is released!" CR
+				THEN
+			THEN
+			ledPin ledState @ digitalWrite
+			reading @ lastbuttonState !
+		key? until
+;
